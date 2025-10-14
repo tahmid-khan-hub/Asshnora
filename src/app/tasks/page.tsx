0b42e48c5513aaf85c5dda61423490f49f801344
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-10-14 19:10:28 +0600

    Updated the instruction section style in task page

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -10,48 +10,48 @@ const TasksPage = () => {
       step: 1,
       title: "Add a Task",
       desc: "Click 'Add Task' to create a new to-do item.",
-      icon: <CiBookmarkPlus size={30} />
+      icon: <CiBookmarkPlus size={40} />
     },
     {
       step: 2,
       title: "Attach Files",
       desc: "Upload photos, PDFs or notes for better context.",
-      icon: <IoMdAttach size={30} />
+      icon: <IoMdAttach size={40} />
     },
     {
       step: 3,
       title: "Ask AI",
       desc: "Let AI organize, prioritize or suggest next actions.",
-      icon: <AiOutlineMessage size={30} />
+      icon: <AiOutlineMessage size={40} />
     },
     {
       step: 4,
       title: "Track Progress",
       desc: "Mark tasks as done and watch your progress grow.",
-      icon: <FaRegCircleCheck size={30} />
+      icon: <FaRegCircleCheck size={40} />
     },
   ];
   return (
     <div>
       <section className="max-w-[1350px] mx-auto px-4 md:px-3 mb-16 text-center">
-        <h2 className="text-3xl font-bold mb-6">
+        <h2 className="text-3xl font-bold mb-3 mt-11 text-black">
           How to Use Smart Task Manager
         </h2>
-        <p className="text-gray-600 mb-10">
-          Follow these simple steps to get started with your AI-powered to-do
-          list.
+        <p className="text-gray-600 mb-16">
+          Follow these simple steps to get started with your AI-powered smart task manager
         </p>
 
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
-          {HowtoUseTask.map(({ step, title, desc }) => (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {HowtoUseTask.map(({ step, title, desc, icon }) => (
             <div
               key={step}
-              className="bg-white rounded-xl shadow-md p-6 hover:shadow-lg transition"
+              className="bg-white text-left rounded-xl shadow-md p-6 hover:shadow-lg transition"
             >
-              <div className="text-4xl font-bold text-blue-500 mb-3">
-                Step {step}
-              </div>
-              <h3 className="text-xl font-semibold mb-2">{title}</h3>
+              <p className="text-blue-500 mb-5">{icon}</p>
+              <h4 className="text-2xl font-bold text-black mb-5">
+                Step-{step}
+              </h4>
+              <h3 className="text-xl font-semibold text-black mb-5">{title}</h3>
               <p className="text-gray-600">{desc}</p>
             </div>
           ))}
