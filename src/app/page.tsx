16c67194ee1bf82ce1fcb5c6efca2782c5d9ddb4
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-10-16 22:36:59 +0600

    Added the privacy section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Features from "./Features/Features";
+import Privacy from "./Privacy/Privacy";
 
 export default function Home() {
   return (
     <div className="text-black max-w-[1350px] mx-auto px-4 md:px-3">
       <Features></Features>
+      <Privacy></Privacy>
     </div>
   );
 }
