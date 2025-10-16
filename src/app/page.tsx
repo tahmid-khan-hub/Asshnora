464bdbbf2ac7def2cabab58ae70e7cf77835e6aa
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-10-16 23:00:13 +0600

    Added the banner section in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Banner from "./Banner/Banner";
 import Features from "./Features/Features";
 import Privacy from "./Privacy/Privacy";
 
 export default function Home() {
   return (
     <div className="text-black max-w-[1350px] mx-auto px-4 md:px-3">
+      <Banner></Banner>
       <Features></Features>
       <Privacy></Privacy>
     </div>
