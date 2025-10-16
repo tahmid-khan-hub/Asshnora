7a5be2797310f0c8b17088a30b1519d93697525e
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-10-16 20:37:51 +0600

    Updated the heading and paragraph of both tasks and scheduler page

--- src/app/scheduler/page.tsx
+++ src/app/scheduler/page.tsx
@@ -34,10 +34,10 @@ const SchedulerPage = () => {
   return (
     <div>
       <section className="max-w-[1350px] mx-auto px-4 md:px-3 mb-16 text-center">
-        <h2 className="text-3xl font-bold mb-3 mt-11 text-black">
+        <h2 className="text-3xl font-bold mb-3 mt-11 text-black relative z-10">
           How to Use Smart Scheduler
         </h2>
-        <p className="text-gray-600 mb-16">
+        <p className="text-gray-600 mb-16 relative z-10">
           Follow these simple steps to get started with your AI-powered Smart
           Scheduler.
         </p>
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -38,10 +38,10 @@ const TasksPage = () => {
     <div>
       {/* How to use it */}
       <section className="max-w-[1350px] mx-auto px-4 md:px-3 mb-16 text-center">
-        <h2 className="text-3xl font-bold mb-3 mt-11 text-black">
+        <h2 className="text-3xl font-bold mb-3 mt-11 text-black relative z-10">
           How to Use Smart Task Manager
         </h2>
-        <p className="text-gray-600 mb-16">
+        <p className="text-gray-600 mb-16 relative z-10">
           Follow these simple steps to get started with your AI-powered smart
           task manager
         </p>
