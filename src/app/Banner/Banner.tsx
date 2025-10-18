3a5d3f8f79af0fc53efa4b320c6809c98b52dfa2
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-10-18 13:26:28 +0600

    Added styles and designed the banner section

--- src/app/Banner/Banner.tsx
+++ src/app/Banner/Banner.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+"use client";
+import React from "react";
+import Link from "next/link";
+import { FaArrowRightLong } from "react-icons/fa6";
+
 
 const Banner = () => {
-    return (
-        <div>
-            banner
-        </div>
-    );
+  return (
+    <section className="text-center py-28 px-4 relative z-10">
+      <h1 className="text-5xl md:text-6xl font-bold mb-4">
+        <span className="bg-gradient-to-r from-blue-600 via-blue-500 to-blue-400 bg-clip-text text-transparent">
+          Smarter Planning Starts Here
+        </span>
+      </h1>
+      <p className="text-lg md:text-xl text-gray-600 mb-12">
+        Let AI organize your tasks and schedule automatically.
+      </p>
+      <Link
+        href="/tasks"
+        className="inline-flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-full font-medium hover:bg-blue-700 transition-all duration-200 shadow-md"
+      >
+        Explore
+        <FaArrowRightLong size={18}/>
+      </Link>
+    </section>
+  );
 };
 
 export default Banner;
