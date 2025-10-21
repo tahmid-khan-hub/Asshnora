6e638bacef6f573704de5e2533b20cd9b51e7e65
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-10-21 17:31:06 +0600

    Added a lottie animation and a divider in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import Link from "next/link";
+import Lottie from "lottie-react";
+import loginLottie from "./login.json"
 import React from "react";
 import { FaGoogle } from "react-icons/fa6";
 
@@ -8,7 +10,13 @@ const LoginPage = () => {
     <div className="min-h-screen flex items-center justify-center px-4 relative z-10">
       <div className="p-8">
         <div className="bg-white rounded-2xl shadow-lg p-10 max-w-md w-full text-center">
-          <h2 className="text-3xl font-semibold text-blue-700 mb-8">
+
+          {/* Lottie Animation */}
+          <div className="w-40 mx-auto mb-4">
+            <Lottie animationData={loginLottie} loop={true} />
+          </div>
+
+          <h2 className="text-3xl font-semibold text-blue-500 mb-8">
             Sign in to Continue
           </h2>
 
@@ -20,10 +28,11 @@ const LoginPage = () => {
             Continue with Google
           </button>
 
-          <div className="mt-8">
+          <div className="divider divider-neutral text-black">OR</div>
+
+          <div className="mt-4 flex items-center justify-center gap-3 w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition">
             <Link
               href="/"
-              className="text-blue-600 hover:underline text-sm font-medium"
             >
               ← Back to Home
             </Link>
