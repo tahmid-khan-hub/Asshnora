11b66714cdce1d3dd83b2c054f51d441b0ff928f
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-10-31 20:39:30 +0600

    Created authOptions and imported it in nextauth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,6 @@
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  pages: {
-    signIn: '/login', // Custom login page
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
